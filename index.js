ddbc9e355e950b46f110b386311d4c5bb5eb4b48
Lisandro Civili <lisandrocivili13@gmail.com>
2024-03-18 18:01:57 -0300

    agregamos y configuramos un puerto

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // archivo principal de NODE
 import express from 'express'
-console.log('hola loco')
+const app = express()
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('port'))
+})
